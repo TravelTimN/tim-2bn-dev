7f065c5cfd1ed33a0f643b0bd0b6613ddfc03633
Tim Nelson <TravelTimN@gmail.com>
2026-07-06 13:32:35 +0100

    Add constants for animations/socials

---
+++ src/constants/animations.ts
@@ -0,0 +1,18 @@
+export const ANIMATION_DURATION = {
+    fast: 0.2,
+    normal: 0.4,
+    slow: 0.7,
+} as const;
+
+export const ANIMATION_DELAY = {
+    none: 0,
+    short: 0.1,
+    medium: 0.2,
+    long: 0.3,
+} as const;
+
+export const ANIMATION_OFFSET = {
+    small: 20,
+    medium: 40,
+    large: 80,
+} as const;
---
+++ src/constants/socials.ts
@@ -0,0 +1,9 @@
+export const SOCIALS = {
+    github: "https://github.com/TravelTimN",
+
+    linkedin: "https://www.linkedin.com/in/travel-tim-nelson",
+
+    email: "mailto:traveltimn@gmail.com",
+
+    website: "https://tim.2bn.dev",
+} as const;
